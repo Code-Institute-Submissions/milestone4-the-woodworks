41cd0d644d3ba30350e174a441fb09ce8d2262de
codewouter <wpc_okhuijsen@hotmail.com>
2020-09-14 19:49:35 +0000

    setting up AWS according to chris' videos

--- the_woodworks/settings.py
+++ the_woodworks/settings.py
@@ -158,7 +158,21 @@ STATICFILES_DIRS = (
 )
 
 # if 'USE_AWS' in os.envrion:
+
+# Bucket config
 AWS_STORAGE_BUCKET_NAME = 'codewouter-the-woodworks'
 AWS_S3_REGION_NAME = 'EU (London)'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Static and media files
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_LOCATION = 'static'
+DEFAULT_FILE_STORAGE = 'custom_storags.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
+
+
+# Override static and media URLs in production
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
