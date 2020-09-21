7575653cd4cf86dfdab9d3b2bbecf1abbb6fdcb5
codewouter <wpc_okhuijsen@hotmail.com>
2020-09-21 17:49:19 +0000

    re-added commented out code

--- cart/views.py
+++ cart/views.py
@@ -23,21 +23,17 @@ def add_to_cart(request, item_id):
     return redirect(redirect_url)
 
 
-# def adjust_cart(request, id):
-#     """
-#     Adjust the quantity of the specified product to the specified
-#     amount. This function was taken from Rob Simons' MS4.
-#     https://github.com/RobSimons1/ms4-ecommerce
-#     """
-#     print(request.POST)
-#     quantity = int(request.POST.get('quantity'))
-#     cart = request.session.get('cart', {})
-#     redirect_url = request.POST.get('redirect_url')
+def adjust_cart(request, item_id):
+    """ Add a quantity of the product to the cart """
 
-#     if quantity > 0:
-#         cart[id] = quantity
-#     else:
-#         cart.pop(id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
 
-#     request.session['cart'] = cart
-#     return redirect(redirect_url)
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
