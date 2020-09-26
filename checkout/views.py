811eb6ccfd9b2a19db807c6138161010aebb0c1a
codewouter <wpc_okhuijsen@hotmail.com>
2020-09-26 09:53:17 +0000

    Added stripe element to checkout page
    using CI's vid (Stripe part 1&2) added stripe element to
    checkout page. Added JS and element to template. Created the js file
    needed and did some styling. Also used stripe's css to style the
    card field on the checkout page.

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HVZ9fCslGk1XQYaKPIJ6BiKao5upyhlYgfjqSrcZk1EUkjcPSEZKUto8ihsUrxc0MWEuorpovPhqeRkgQECqY3t00fPkDWgyA',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
