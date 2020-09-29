6834c58e78ade0543c869bb2ba9248431b621bba
codewouter <wpc_okhuijsen@hotmail.com>
2020-09-29 19:44:44 +0000

    Created poll model

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    product_type = models.CharField(max_length=254, default='')
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.product_type
