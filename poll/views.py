76720e7be22444fec0b4e97f260449bca51beb00
codewouter <wpc_okhuijsen@hotmail.com>
2020-09-29 20:01:53 +0000

    Made migrations; added model to admin; setup poll.view.py

--- poll/views.py
+++ poll/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Poll
 
 
 # Create your views here.
 def view_poll(request):
 
-    return render(request, 'poll/poll.html')
+    poll_product_list = Poll.objects.all()
+    template = 'poll/poll.html'
+    context = {
+        poll_product_list: poll_product_list
+    }
+
+    return render(request, template, context)
