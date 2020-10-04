e740b36db8fa1fc02afdbacc7a4bcf7653b53c8b
codewouter <wpc_okhuijsen@hotmail.com>
2020-10-04 10:24:43 +0000

    Finished reviews app for now.

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Review
 from store.models import Product
+from .forms import ReviewForm
 
 
 # Create your views here.
@@ -22,22 +23,32 @@ def view_reviews(request, product_id):
 
 
 def add_review(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    selected_product = get_object_or_404(Product, pk=product_id)
+    review_list = Review.objects.all().filter(product=product)
+    print(selected_product.id)
     """
     When the add review button on the page is pressed the function is called
     and given the product_id as an argument.
     """
-    selected_product = get_object_or_404(Product, pk=product_id)
-    print(selected_product.id)
+
+    if request.method == "POST":
+        new_review = ReviewForm(request.POST)
+        if new_review.is_valid():
+            new_review.instance.product = product
+            new_review.instance.reviewer = request.POST.get('reviewer')
+            new_review.instance.review_text = request.POST.get('review_text')
+            new_review.save()
+
+            template = 'reviews/reviews.html'
+            context = {
+                'review_list': review_list,
+                'product': product
+            }
+
+            return render(request, template, context)
+    else:
+        new_review = ReviewForm()
+
     redirect_url = request.POST.get('redirect_url')
     return redirect(redirect_url)
-
-
-# def add_vote(request, poll_product_id):
-#     product_type = get_object_or_404(Poll, pk=poll_product_id)
-#     print(product_type.product_type)
-#     print(product_type.votes)
-#     redirect_url = request.POST.get('redirect_url')
-#     product_type.votes += 1
-#     product_type.save()
-
-#     return redirect(redirect_url)
